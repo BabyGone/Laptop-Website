74a85b81428830eea79767cae9043f4668a3ec29
BabyGone <90824775+BabyGone@users.noreply.github.com>
2022-03-25 13:58:02 +0700

    Delete Laptop directory

--- Laptop/admin/partials/footer.php
+++
@@ -1,7 +0,0 @@
-	<div class="footer">
-		<div class="wrapper">
-			<p class="text-center">2022 All rights reserved, Best Laptop. Developed by Nhom 17 - 61TH5.</p>
-		</div>
-	</div>
-</body>
-</html>
--- Laptop/admin/partials/login-check.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	if (!isset($_SESSION['user'])) 
-	{
-		$_SESSION['no-login-message'] = "<div class='error text-center'>Vui lòng đăng nhập để vào Admin!</div>";
-		header('location:'.SITEURL.'admin/login.php');
-	}
-?>
